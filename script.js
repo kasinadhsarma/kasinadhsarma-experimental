f6b1902da9e0becdd9b145d8061fba2a475cc5b2
swayampakula.v.s.s.pavanakasinadha sarma <81065703+kasinadhsarma@users.noreply.github.com>
2024-02-25 10:55:38 +0530

    update

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-// Inside your A-Frame scene initialization, if you have it
-const sphere = document.querySelector('a-sphere'); // Get the sphere
-sphere.addEventListener('loaded', () => { 
-    console.log('Sphere loaded');
-    if (sphere.components.animation) {
-        console.log('Animation component is present');
-    } else {
-        console.error('Animation component is missing!');
-    }
-}); 
+const earth = document.getElementById('earth');
+const info = document.getElementById('info');
+
+earth.classList.toggle('white-mode');
+info.classList.remove('hidden');
+
